e541824dcb3c08b401900a076c81f8ae85d780eb
Ryvqi <rifqikhabibi03@gmail.com>
2023-04-03 15:07:47 +0700

    membuat input

--- ActivityStructureV2_169/ActivityStructureV2_169.cpp
+++ ActivityStructureV2_169/ActivityStructureV2_169.cpp
@@ -1,20 +1,35 @@
-// ActivityStructureV2_169.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
+struct AlamatDetail
 {
-    std::cout << "Hello World!\n";
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string NIM;
+	string Nama;
+	AlamatDetail Alamat;
+	int Umur;
+};
+
+int main() 
+{
+	Mahasiswa mhs;
+
+	cout << " Masukan nim : ";
+	cin >> mhs.NIM;
+	cout << " Masukan nama : ";
+	cin >> mhs.Nama;
+	cout << "Alamat : ";
+	cout << "\tNama desa : ";
+	cin >> mhs.Alamat.desa;
+	cout << "\tNama kota : ";
+	cin >> mhs.Alamat.kota;
+	cout << "Masukan umur : ";
+	cin >> mhs.Umur;
+
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
