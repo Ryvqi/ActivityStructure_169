beb2af5ce5459db59e698f681094aa75c922666c
Ryvqi <rifqikhabibi03@gmail.com>
2023-04-03 15:26:09 +0700

    membuat prosedure

--- ActivityStructureV3_169/ActivityStructureV3_169.cpp
+++ ActivityStructureV3_169/ActivityStructureV3_169.cpp
@@ -1,20 +1,36 @@
-// ActivityStructureV3_169.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
+struct AlamatDetail
 {
-    std::cout << "Hello World!\n";
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa {
+	char nim[3];
+	char nama[20];
+	AlamatDetail alamat;
+	int umur;
+};
+
+int main(){
+
+	Mahasiswa mhs[3];
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Masukan nim : ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "Masukan nama : ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat : ";
+		cout << "\tNama desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\nNama kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "Masukan Umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+	}
+  
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
