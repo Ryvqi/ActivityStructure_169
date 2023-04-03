2988964e287056fb2f0eb98fb4ff64922ee18bf8
Ryvqi <rifqikhabibi03@gmail.com>
2023-04-03 14:41:13 +0700

    membuat data mahasiswa

--- ActivityStructure_169/ActivityStructure_169.cpp
+++ ActivityStructure_169/ActivityStructure_169.cpp
@@ -5,10 +5,26 @@ struct Mahasiswa {
 	string nim;
 	string nama;
 	string alamat;
-	string umur;
+	int umur;
 };
 
 int main()
 {
+	Mahasiswa mhs1, mhs2;
 
+	mhs1.nim = "20220140169";
+	mhs1.nama = "Rifqi";
+	mhs1.alamat = "Jogja";
+	mhs1.umur = 19;
+
+	cout << "Masukan NIM : ";
+	cin >> mhs2.nim;
+	cout << "Masukan Nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukan Alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukan Umur : ";
+	cin >> mhs2.umur;
+
+	
 }
